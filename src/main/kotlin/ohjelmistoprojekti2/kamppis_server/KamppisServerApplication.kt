21f8d4e947a392b5cdff0ff54cb85b7b9617ec24
devaajanne <devaajanne@gmail.com>
2025-01-21 12:52:52 +0200

    Initial commit

---
+++ src/main/kotlin/ohjelmistoprojekti2/kamppis_server/KamppisServerApplication.kt
@@ -0,0 +1,11 @@
+package ohjelmistoprojekti2.kamppis_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KamppisServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KamppisServerApplication>(*args)
+}
