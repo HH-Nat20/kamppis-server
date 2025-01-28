6d04261040dd153e9894ac678852e6e3065f446b
Phoolis <carlsonps@gmail.com>
2025-01-28 20:33:24 +0200

    Add unit test for /api/users/ endpoint

--- src/test/kotlin/ohjelmistoprojekti2/kamppis_server/UserRestControllerTests.kt
+++ src/test/kotlin/ohjelmistoprojekti2/kamppis_server/UserRestControllerTests.kt
@@ -1,4 +1,44 @@
 package ohjelmistoprojekti2.kamppis_server
 
-class UserRestControllerTests {
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import ohjelmistoprojekti2.kamppis_server.api.UserRestController
+import ohjelmistoprojekti2.kamppis_server.domain.User
+import ohjelmistoprojekti2.kamppis_server.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import java.time.LocalDate
+import kotlin.test.Test
+
+@WebMvcTest(UserRestController::class)
+class UserRestControllerTests(@Autowired private val mockMvc: MockMvc) {
+
+    @MockkBean
+    lateinit var userRepository: UserRepository;
+
+    @Test
+    fun `List all users`() {
+        val bobJohnson = User(
+            email = "bob.johnson@example.com",
+            firstName = "Bob",
+            lastName = "Johnson",
+            dateOfBirth = LocalDate.of(1985, 11, 22)
+        )
+        val charlieDavis = User(
+            email = "charlie.davis@example.com",
+            firstName = "Charlie",
+            lastName = "Davis",
+            dateOfBirth = null // Date of birth not provided
+        )
+        every { userRepository.findAll() } returns listOf(bobJohnson, charlieDavis)
+        mockMvc.perform(get("/api/user/").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk)
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].email").value(bobJohnson.email))
+            .andExpect(jsonPath("\$.[1].email").value(charlieDavis.email))
+    }
 }
