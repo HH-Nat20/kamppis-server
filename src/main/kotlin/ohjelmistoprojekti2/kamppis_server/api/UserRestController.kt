f4a61e20ea65cd8a5883caa17c90758b4ed2ce49
Phoolis <carlsonps@gmail.com>
2025-01-28 19:59:04 +0200

    Fix null issues in UserRestController

--- src/main/kotlin/ohjelmistoprojekti2/kamppis_server/api/UserRestController.kt
+++ src/main/kotlin/ohjelmistoprojekti2/kamppis_server/api/UserRestController.kt
@@ -1,22 +1,23 @@
 package ohjelmistoprojekti2.kamppis_server.api
 
+import ohjelmistoprojekti2.kamppis_server.domain.User
 import ohjelmistoprojekti2.kamppis_server.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
-import kotlin.jvm.optionals.getOrElse
 
 @RestController
 @RequestMapping("/api/user")
 class UserRestController(private val repository: UserRepository){
 
     @GetMapping("/")
-    fun findAll() = repository.findAll()
+    fun findAll(): MutableIterable<User> = repository.findAll()
 
     @GetMapping("/{id}")
-    fun findUserById(@PathVariable id: Long) = repository.findById(id).getOrElse {
-        throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist") }
+    fun findUserById(@PathVariable id: Long) = repository.findByIdOrNull(id)
+        ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
 }
