4e326e29639bdb348308c31779b0d17b4f7a58f5
Phoolis <carlsonps@gmail.com>
2025-01-28 21:46:55 +0200

    Add unit test for /api/user/{id} endpoint (and test new GitHub workflow)

--- src/test/kotlin/ohjelmistoprojekti2/kamppis_server/UserRestControllerTests.kt
+++ src/test/kotlin/ohjelmistoprojekti2/kamppis_server/UserRestControllerTests.kt
@@ -7,6 +7,7 @@ import ohjelmistoprojekti2.kamppis_server.domain.User
 import ohjelmistoprojekti2.kamppis_server.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.MockMvc
@@ -40,5 +41,20 @@ class UserRestControllerTests(@Autowired private val mockMvc: MockMvc) {
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("\$.[0].email").value(bobJohnson.email))
             .andExpect(jsonPath("\$.[1].email").value(charlieDavis.email))
+    }
+
+    @Test
+    fun `Find user by id`() {
+        val bobJohnson = User(
+            email = "bob.johnson@example.com",
+            firstName = "Bob",
+            lastName = "Johnson",
+            dateOfBirth = LocalDate.of(1985, 11, 22)
+        )
+        every { userRepository.findByIdOrNull(any()) } returns bobJohnson
+        mockMvc.perform(get("/api/user/1").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.email").value(bobJohnson.email))
     }
 }
