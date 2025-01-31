fee5319c6c957692cc786b8c85f9a281d74cabc2
devaajanne <devaajanne@gmail.com>
2025-01-31 15:28:24 +0200

    Add function to return all users from repository to QueryService

--- src/main/kotlin/ohjelmistoprojekti2/kamppis_server/service/QueryService.kt
+++ src/main/kotlin/ohjelmistoprojekti2/kamppis_server/service/QueryService.kt
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service
 import ohjelmistoprojekti2.kamppis_server.repository.UserRepository
 
 @Service
-class QueryService {
+class QueryService(val userRepository: UserRepository) {
 
+    fun findAllUsersFromRepository(): List<User> {
+        return userRepository.findAll().toList();
+    }
 }
