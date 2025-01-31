8b09e8fb80e6114bdf2d75a8880a6bf8914ef842
devaajanne <devaajanne@gmail.com>
2025-01-31 14:46:24 +0200

    Add Service package and QueryService class

---
+++ src/main/kotlin/ohjelmistoprojekti2/kamppis_server/service/QueryService.kt
@@ -0,0 +1,11 @@
+package ohjelmistoprojekti2.kamppis_server.service
+
+import ohjelmistoprojekti2.kamppis_server.domain.User
+import org.springframework.stereotype.Service
+
+import ohjelmistoprojekti2.kamppis_server.repository.UserRepository
+
+@Service
+class QueryService {
+
+}
