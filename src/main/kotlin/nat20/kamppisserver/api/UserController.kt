2fec210f4be10e18c4efdd76b848ada395521a84
devaajanne <devaajanne@gmail.com>
2025-02-04 14:24:27 +0200

    Update endpoint to UserController and functionality to GET all users that meet search from UserRepository with SQL

--- src/main/kotlin/nat20/kamppisserver/api/UserController.kt
+++ src/main/kotlin/nat20/kamppisserver/api/UserController.kt
@@ -1,14 +1,12 @@
 package nat20.kamppisserver.api
 
+import com.fasterxml.jackson.databind.JsonNode
 import nat20.kamppisserver.domain.User
 import nat20.kamppisserver.repository.UserRepository
 import nat20.kamppisserver.service.QueryService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
@@ -23,7 +21,7 @@ class UserController(private val repository: UserRepository, private val querySe
         ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
 
     @GetMapping("/query")
-    fun findAllUsersWithSQL(): MutableIterable<User> {
-        return queryService.findAllUsersWithSQL()
+    fun findUsersThatMeetCriteria(@RequestBody criteria: JsonNode): MutableIterable<User> {
+        return queryService.findUsersThatMeetCriteria(criteria)
     }
 }
--- src/main/kotlin/nat20/kamppisserver/service/QueryService.kt
+++ src/main/kotlin/nat20/kamppisserver/service/QueryService.kt
@@ -1,14 +1,23 @@
 package nat20.kamppisserver.service
 
-import nat20.kamppisserver.domain.User
 import org.springframework.stereotype.Service
+import com.fasterxml.jackson.databind.JsonNode
 
 import nat20.kamppisserver.repository.UserRepository
+import nat20.kamppisserver.domain.User
 
 @Service
 class QueryService(private val userRepository: UserRepository) {
 
-    fun findAllUsersWithSQL(): MutableIterable<User> {
-        return userRepository.findAllUsersWithSQL()
+    fun findUsersThatMeetCriteria(criteria: JsonNode): MutableIterable<User> {
+        /* We first destruct the GET request body that holds the criteria to individual parameters
+        * Then we pass these parameters to the SQL query
+        * NOTE! Criteria in the request body must be destructed to individual search parameter variables!
+        * -> val criteria: type = criteria["criteriaKeyInRequestBody"].asType()
+        * NOTE! All individual search parameter variables must be given as function parameters below!*/
+
+        val id: Long = criteria["searchMadeByUserId"].asLong()
+
+        return userRepository.findUsersThatMeetCriteria(id)
     }
 }
