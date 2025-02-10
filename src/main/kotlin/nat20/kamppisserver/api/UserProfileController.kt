32a0548d30396f2e367479bd32c88801ab6045e3
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2025-02-10 11:30:36 +0200

    Add delete functionality to UserProfileController

--- src/main/kotlin/nat20/kamppisserver/api/UserProfileController.kt
+++ src/main/kotlin/nat20/kamppisserver/api/UserProfileController.kt
@@ -2,12 +2,9 @@ package nat20.kamppisserver.api
 
 import nat20.kamppisserver.domain.UserProfile
 import nat20.kamppisserver.service.UserProfileService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  * REST controller for User Profile.
@@ -26,5 +23,17 @@ class UserProfileController(private val service: UserProfileService) {
     @PutMapping("/{id}")
     fun updateUserProfile(@RequestBody userProfile: UserProfile, @PathVariable id: Long): ResponseEntity<UserProfile>
         = ResponseEntity.ok(service.update(userProfile, id))
+
+    /**
+     * Soft deletes user profile.
+     *
+     * @param id the id of the profile to be deleted.
+     * @return ResponseEntity with status code 204 NO CONTENT.
+     */
+    @DeleteMapping("/{id")
+    fun deleteById(@PathVariable id: Long): ResponseEntity<Void> {
+        service.delete(id)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
+    }
 
 }
