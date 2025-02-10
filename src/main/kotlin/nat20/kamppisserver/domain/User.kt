e0e0b538dc2d53691c69f77dde3a29f85fdc0181
devaajanne <devaajanne@gmail.com>
2025-02-10 15:11:03 +0200

    Updated User, UserProfile, UserHabit, UserInterest entity classes

--- src/main/kotlin/nat20/kamppisserver/domain/User.kt
+++ src/main/kotlin/nat20/kamppisserver/domain/User.kt
@@ -1,35 +1,20 @@
 package nat20.kamppisserver.domain
 
 import jakarta.persistence.*
-import java.time.LocalDate
 
 /**
  * Entity class for User.
- * @OneToMany relationship to UserHabit and UserInterest.
  */
 @Entity
 @Table(name = "users")
 class User(
-    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
-    @JoinColumn(name = "user_id")
-    var userHabits: MutableList<UserHabit> = mutableListOf(),
-
-    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
-    @JoinColumn(name = "user_id")
-    var userInterests: MutableList<UserInterest> = mutableListOf(),
-
     var email: String,
-    var firstName: String,
-    var lastName: String,
 
-    // user is not required to reveal their age
-    var dateOfBirth: LocalDate? = null,
-    // .
-    // .  ADD FIELDS HERE AS REQUIRED
-    // .
+    /*
+    * ADD FIELDS HERE AS REQUIRED
+    * */
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
-
-    )
+)
