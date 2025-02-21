9bdfd945a1bb2d29bc205cb15bbdc3fc6a8e5bcf
devaajanne <devaajanne@gmail.com>
2025-02-21 17:12:06 +0200

    Fix UserProfile query to accept and get querydate as parameter

--- src/main/kotlin/nat20/kamppisserver/repository/UserProfileRepository.kt
+++ src/main/kotlin/nat20/kamppisserver/repository/UserProfileRepository.kt
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import nat20.kamppisserver.domain.UserProfile
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import java.time.LocalDate
 
 
 interface UserProfileRepository: JpaRepository<UserProfile, Long> {
@@ -24,20 +25,18 @@ interface UserProfileRepository: JpaRepository<UserProfile, Long> {
     * -> We don't want to include the user who made the query in the results,
     * -> so we exclude them from the returned users
     *
-    * 4) (DATEDIFF(year, "date_of_birth", CURDATE()) + CASE WHEN DATEADD(year, DATEDIFF(year, "date_of_birth", CURDATE()), "date_of_birth") > CURDATE() THEN -1 ELSE 0 END) BETWEEN COALESCE(:minAgePreference, 0) AND COALESCE(:maxAgePreference, 1000)
+    * 4) (DATEDIFF(YEAR, "date_of_birth", :queryDate) + CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR, "date_of_birth", :queryDate), "date_of_birth") > :queryDate THEN -1 ELSE 0 END) BETWEEN COALESCE(:minAgePreference, 0) AND COALESCE(:maxAgePreference, 1000)
     * -> We calculate user's age based on their date of birth
     * -> We remove 1 year if the user's has not yet had their birthday this year
     * -> We include only those users who fit min and max age preferences (inclusive)
     * -> If user has not set any age preferences (i.e. null), values 0 and 1000 are used to include user profiles of all ages
     *
     * More criteria and parameters will be added */
-
-    // PostgreSQL query for when we have external database --> TODO: figure out how to use this query when connected to PostgreSQL database, and the other query when using runtime database
-    //@Query(value= "SELECT * FROM \"user_profiles\" WHERE (\"id\" != :id) AND (EXTRACT(YEAR FROM AGE(current_date, \"date_of_birth\"))) BETWEEN COALESCE(:minAgePreference, 0) AND COALESCE(:maxAgePreference, 1000))", nativeQuery = true)
-
-    @Query(value= "SELECT * FROM \"user_profiles\" WHERE (\"id\" != :id) AND (DATEDIFF(YEAR, \"date_of_birth\", CURDATE()) + CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR, \"date_of_birth\", CURDATE()), \"date_of_birth\") > CURDATE() THEN -1 ELSE 0 END) BETWEEN COALESCE(:minAgePreference, 0) AND COALESCE(:maxAgePreference, 1000)", nativeQuery = true)
+    
+    @Query(value= "SELECT * FROM \"user_profiles\" WHERE (\"id\" != :id) AND ((DATEDIFF(YEAR, \"date_of_birth\", :queryDate) + CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR, \"date_of_birth\", :queryDate), \"date_of_birth\") > :queryDate THEN -1 ELSE 0 END) BETWEEN COALESCE(:minAgePreference, 0) AND COALESCE(:maxAgePreference, 1000) )", nativeQuery = true)
     fun findUserProfilesThatMeetCriteria(
         @Param("id") id: Long?,
+        @Param("queryDate") queryDate: LocalDate,
         @Param("minAgePreference") minAgePreference: Int?,
         @Param("maxAgePreference") maxAgePreference: Int?): MutableIterable<UserProfile>
 
