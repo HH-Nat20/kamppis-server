08e1f62b3c71cc72df8076862e204c71736d100b
Phoolis <carlsonps@gmail.com>
2025-02-21 16:51:29 +0200

    Fix infinite loop in user matches

--- src/main/kotlin/nat20/kamppisserver/domain/User.kt
+++ src/main/kotlin/nat20/kamppisserver/domain/User.kt
@@ -1,5 +1,6 @@
 package nat20.kamppisserver.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.*
 
 /**
@@ -16,7 +17,7 @@ class User(
     /*
     * ADD FIELDS HERE AS REQUIRED
     * */
-
+    @JsonIgnore
     @ManyToMany(mappedBy = "users") // This makes it bidirectional
     var matches: MutableSet<Match> = mutableSetOf(),
 
