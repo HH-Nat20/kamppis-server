750f95e97328c2965fbb3167d05fc890cc258163
Phoolis <carlsonps@gmail.com>
2025-02-25 15:09:41 +0200

    Refactor GlobalExceptionHandler

--- src/main/kotlin/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/exception/GlobalExceptionHandler.kt
@@ -8,21 +8,17 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException::class)
     fun handleEntityNotFound(ex: EntityNotFoundException){
-        throw ex
     }
 
     @ExceptionHandler(DuplicateSwipeException::class)
     fun handleDuplicateSwipe(ex: DuplicateSwipeException){
-        throw ex
     }
 
     @ExceptionHandler(DuplicateMatchException::class)
     fun handleDuplicateMatch(ex: DuplicateMatchException) {
-        throw ex
     }
 
     @ExceptionHandler(InvalidRequestException::class)
     fun handleInvalidRequest(ex: InvalidRequestException) {
-        throw ex
     }
 }
