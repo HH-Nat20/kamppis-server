9d9d7551b668dc702a68988abb60cf311f79090c
Phoolis <carlsonps@gmail.com>
2025-03-12 19:37:20 +0200

    Refactor API health and dbHealth endpoints to use ResponseEntities for openApi

--- src/main/kotlin/nat20/kamppisserver/api/HealthController.kt
+++ src/main/kotlin/nat20/kamppisserver/api/HealthController.kt
@@ -1,5 +1,6 @@
 package nat20.kamppisserver.api
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class HealthController {
 
     @GetMapping
-    fun serverHealth(): Map<String, String> {
-        return mapOf("status" to "ok")
+    fun serverHealth(): ResponseEntity<Map<String, String>> {
+        return ResponseEntity.ok(mapOf("status" to "ok"))
     }
 }
