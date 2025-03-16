4d74344ccb0ad0401058a2190fb43570c71ca685
Bminor87 <jessehellman@gmail.com>
2025-03-17 00:29:09 +0200

    Add missing loadAll import

--- src/main/kotlin/nat20/kamppisserver/storage/FileSystemStorageService.kt
+++ src/main/kotlin/nat20/kamppisserver/storage/FileSystemStorageService.kt
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile
 import java.io.IOException
 import java.nio.file.*
 import java.util.UUID
+import java.util.stream.Stream
 import org.springframework.util.FileSystemUtils
 import java.net.MalformedURLException
 
