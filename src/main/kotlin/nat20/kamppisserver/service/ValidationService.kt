815f234421a7803e0d0ca8f87f75b2edcbea5097
devaajanne <devaajanne@gmail.com>
2025-03-18 10:50:35 +0200

    Add RoommatePreferences table and custom annotation + validator

--- src/main/kotlin/nat20/kamppisserver/service/ValidationService.kt
+++ src/main/kotlin/nat20/kamppisserver/service/ValidationService.kt
@@ -1,5 +1,13 @@
 package nat20.kamppisserver.service
 
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import nat20.kamppisserver.domain.RoommatePreferences
+import org.springframework.stereotype.Component
+import kotlin.reflect.KClass
+
 /**
  * Service class for input validation and sanitization. Can be used to sanitize
  * user inputs in forms and search bars, GET and POST requests, cookies, and more.
@@ -21,4 +29,41 @@ class ValidationService {
         return sanitized.matches(validPattern) && !riskyPattern.containsMatchIn(sanitized)
     }
 
+    /**
+     * Custom annotation to check that min age preference is not greater than max age preference
+     * Used in RoommatePreferences entity
+     */
+    @Constraint(validatedBy = [AgePreferenceValidator::class])
+    @Target(AnnotationTarget.CLASS)
+    @Retention(AnnotationRetention.RUNTIME)
+    annotation class ValidAgePreferences(
+        val message: String = "Min age preference must be less than max age preference",
+        val groups: Array<KClass<*>> = [], //Mandatory, can be left empty for simple validations
+        val payload: Array<KClass<out Payload>> = [] //Mandatory, can be left empty for simple validations
+    )
+
+    /**
+     * Custom validator to check that min age preference is not greater than max age preference
+     * Used in RoommatePreferences entity
+     */
+    @Component
+    class AgePreferenceValidator: ConstraintValidator<ValidAgePreferences, RoommatePreferences> {
+        override fun isValid(preferences: RoommatePreferences, context: ConstraintValidatorContext): Boolean {
+            if (preferences.minAgePreference >= preferences.maxAgePreference) {
+                // Disables default validation message
+                context.disableDefaultConstraintViolation()
+
+                // Creates new custom error message (does not automatically use annotation's message)
+                context.buildConstraintViolationWithTemplate("Min age preference must be less than max age preference")
+                    // Binds the error message to the correct field
+                    .addPropertyNode("minAgePreference")
+                    // Finalizes error configuration
+                    .addConstraintViolation()
+
+                return false // Validation fails
+            }
+
+            return true // Validation passes
+        }
+    }
 }
