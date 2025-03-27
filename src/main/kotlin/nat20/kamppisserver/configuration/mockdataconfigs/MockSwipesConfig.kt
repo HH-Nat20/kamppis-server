fa294a745f16675951c80128d4404e2797fa7802
devaajanne <devaajanne@gmail.com>
2025-03-27 09:45:37 +0200

    Refactor mock swipes, now easier to update

--- src/main/kotlin/nat20/kamppisserver/configuration/mockdataconfigs/MockSwipesConfig.kt
+++ src/main/kotlin/nat20/kamppisserver/configuration/mockdataconfigs/MockSwipesConfig.kt
@@ -14,15 +14,26 @@ class MockSwipesConfig {
         swipeRepository: SwipeRepository
     ) {
         // These are swipes that profiles have given to each other
-        val swipes = listOf(
+
+        // User profiles who have swiped other user profiles
+        val userToUserSwipes = listOf(
             Swipe(profileRepository.findById(1L).get(), profileRepository.findById(2L).get(), true),
             Swipe(profileRepository.findById(1L).get(), profileRepository.findById(3L).get(), true),
             Swipe(profileRepository.findById(2L).get(), profileRepository.findById(1L).get(), true),
             Swipe(profileRepository.findById(3L).get(), profileRepository.findById(1L).get(), true),
+        )
+
+        // User profiles who have swiped room profiles
+        val userToRoomSwipes = listOf(
             Swipe(profileRepository.findById(1L).get(), profileRepository.findById(79L).get(), true),
+        )
+
+        // Room profiles who have swiped user profiles
+        val roomToUserSwipes = listOf(
             Swipe(profileRepository.findById(79L).get(), profileRepository.findById(1L).get(), true),
         )
 
-        swipeRepository.saveAll(swipes)
+        // Save all mock swipes to the database
+        swipeRepository.saveAll(userToUserSwipes + userToRoomSwipes + roomToUserSwipes)
     }
 }
