2960cc7c79bb9116025efb9ee4091590b2d8c971
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2025-03-28 15:38:46 +0200

    JPA/Hibernate already implements input sanitization and prepared statements, no need to make our own custom implementation as of now

--- src/main/kotlin/nat20/kamppisserver/service/ValidationService.kt
+++ src/main/kotlin/nat20/kamppisserver/service/ValidationService.kt
@@ -15,21 +15,6 @@ import kotlin.reflect.KClass
 class ValidationService {
 
     /**
-     * Validates String values by checking for whitelisted characters.
-     */
-    fun isValidString(string: String): Boolean {
-        val sanitized = string.trim()
-
-        // Only allow letters, spaces, apostrophes, and hyphens
-        val validPattern = Regex("^[a-zA-Z' -]+$")
-
-        // Check for control characters, null bytes, or other risky characters
-        val riskyPattern = Regex("[\\x00-\\x1F<>\"&%;(){}=]")
-
-        return sanitized.matches(validPattern) && !riskyPattern.containsMatchIn(sanitized)
-    }
-
-    /**
      * Custom annotation to check that min age preference is not greater than max age preference
      * Used in RoommatePreferences entity
      */
