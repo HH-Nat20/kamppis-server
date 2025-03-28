9897fc3f2bb6f1653f3e27d8bb7a3ce43c0a434a
devaajanne <devaajanne@gmail.com>
2025-03-28 09:14:27 +0200

    Update room profile tests

--- src/test/kotlin/nat20/kamppisserver/repository/RoomProfileRepositoryTests.kt
+++ src/test/kotlin/nat20/kamppisserver/repository/RoomProfileRepositoryTests.kt
@@ -22,13 +22,19 @@ class RoomProfileRepositoryTests @Autowired constructor(
     val userRepository: UserRepository,
     val roomPreferenceRepository: RoomPreferenceRepository,
     val roomProfileRepository: RoomProfileRepository,
+    val swipeRepository: SwipeRepository
 ){
     @Test
     fun `should return RoomProfiles whose rent is below or at user's max rent criteria`() {
         val numberOfMatchingRoomProfiles: Int = 18
-        val user: User? = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
-        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user!!.id!!, UserStatus.ACTIVE)
+        val user: User = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
+            ?: fail("Expected User but found null")
+        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user.id!!, UserStatus.ACTIVE)
             ?: fail("Expected RoomPreference but found null")
+
+        // We delete the swipes because we don't want them to interfere the test
+        // If not, the query filters out swiped profiles and max rent test fails
+        swipeRepository.deleteAll()
 
         // Set other criteria to null so that they do not affect the max rent filtering
         val hasPrivateRoom = null;
@@ -50,9 +56,14 @@ class RoomProfileRepositoryTests @Autowired constructor(
     @Test
     fun `should return RoomProfiles that match user's room privacy criteria`() {
         val numberOfMatchingRoomProfiles: Int = 14
-        val user: User? = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
-        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user!!.id!!, UserStatus.ACTIVE)
+        val user: User = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
+            ?: fail("Expected User but found null")
+        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user.id!!, UserStatus.ACTIVE)
             ?: fail("Expected RoomPreference but found null")
+
+        // We delete the swipes because we don't want them to interfere the test
+        // If not, the query filters out swiped profiles and privacy criteria test fails
+        swipeRepository.deleteAll()
 
         // Set other criteria to null so that they do not affect the room privacy filtering
         val maxRent = null
@@ -74,9 +85,14 @@ class RoomProfileRepositoryTests @Autowired constructor(
     @Test
     fun `should return RoomProfiles for flats whose total roommate count is below or at user's max roommate criteria`() {
         val numberOfMatchingRoomProfiles: Int = 14
-        val user: User? = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
-        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user!!.id!!, UserStatus.ACTIVE)
+        val user: User = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
+            ?: fail("Expected User but found null")
+        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user.id!!, UserStatus.ACTIVE)
             ?: fail("Expected RoomPreference but found null")
+
+        // We delete the swipes because we don't want them to interfere the test
+        // If not, the query filters out swiped profiles and max roommate test fails
+        swipeRepository.deleteAll()
 
         // Set other criteria to null so that they do not affect the max roommate filtering
         val maxRent = null
@@ -95,14 +111,17 @@ class RoomProfileRepositoryTests @Autowired constructor(
         assertEquals(numberOfMatchingRoomProfiles, listOfRoomProfiles.count())
     }
 
-
-
     @Test
     fun `should return RoomProfiles for flats whose location is in user's location preferences`() {
         val numberOfMatchingRoomProfiles: Int = 18
-        val user: User? = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
-        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user!!.id!!, UserStatus.ACTIVE)
+        val user: User = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
+            ?: fail("Expected User but found null")
+        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user.id!!, UserStatus.ACTIVE)
             ?: fail("Expected RoomPreference but found null")
+
+        // We delete the swipes because we don't want them to interfere the test
+        // If not, the query filters out swiped profiles and location preference test fails
+        swipeRepository.deleteAll()
 
         // Set other criteria to null so that they do not affect the location filtering
         val maxRent = null
@@ -149,18 +168,21 @@ class RoomProfileRepositoryTests @Autowired constructor(
 
     @Test
     fun `should return the correct amount of room profiles when all criteria are used`() {
-        val numberOfMatchingRoomProfiles: Int = 10
-        val user: User? = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
-        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user!!.id!!, UserStatus.ACTIVE)
+        val numberOfMatchingRoomProfiles: Int = 8
+        val user: User = userRepository.findByIdAndStatus(27L, UserStatus.ACTIVE)
+            ?: fail("Expected User but found null")
+        val roomPreference: RoomPreference = roomPreferenceRepository.findByUserIdAndStatus(user.id!!, UserStatus.ACTIVE)
             ?: fail("Expected RoomPreference but found null")
 
+        // Query parameters (=user's search criteria) are selected from the user's room preferences
+        // Swipes are taken into account
         val listOfRoomProfiles: Iterable<RoomProfile> =
             roomProfileRepository.findRoomProfilesThatMeetCriteria(
                 user.id!!,
                 roomPreference.maxRent,
                 roomPreference.hasPrivateRoom,
                 roomPreference.maxRoommates,
-                roomPreference.locationPreferences!!.map {it.name}.toMutableList()
+                roomPreference.locationPreferences?.map {it.name}
             )
 
         assertEquals(numberOfMatchingRoomProfiles, listOfRoomProfiles.count())
