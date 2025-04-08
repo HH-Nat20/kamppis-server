a06b87f4932c56139f5f96668c90c3361c44b64a
devaajanne <devaajanne@gmail.com>
2025-04-08 18:30:52 +0300

    Add KDoc comment to controller

--- src/main/kotlin/nat20/kamppisserver/api/UserProfileController.kt
+++ src/main/kotlin/nat20/kamppisserver/api/UserProfileController.kt
@@ -79,6 +79,14 @@ class UserProfileController(private val service: UserProfileService,
         return ResponseEntity(userProfileList, HttpStatus.OK)
     }
 
+    /**
+     * Finds all the user profiles that have swiped user's room profile(s)
+     *
+     * @param id the id of the user who initiated the query.
+     * @return ResponseEntity with map (key: roomProfileId, value: list of userProfiles) and status code 200 OK if user profiles have swiped the room profile.
+     * @return ResponseEntity with map (key: roomProfileId, value: empty list of userProfiles) and status code 200 OK if no user profiles have swiped the room profile.
+     * @return ResponseEntity with status code 204 NO_CONTENT if the user has no room profiles.
+     */
     @GetMapping("/{userId}/roomswipesquery")
     fun findUserProfilesWhoHaveSwipedUsersRoom(@PathVariable userId: Long): ResponseEntity<Map<Long, List<UserProfileDTO>>> {
         val userProfileMap: Map<Long, List<UserProfileDTO>> = queryService.findUserProfilesThatHaveSwipedUsersRoom(userId)
