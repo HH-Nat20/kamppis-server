e157db5e86e0c47990bc7df61120453572661c2c
devaajanne <devaajanne@gmail.com>
2025-05-02 12:18:46 +0300

    Refactor to fix single responsibility principle violation: invite controller and service

--- src/main/kotlin/nat20/kamppisserver/api/InviteController.kt
+++ src/main/kotlin/nat20/kamppisserver/api/InviteController.kt
@@ -1,83 +1,26 @@
 package nat20.kamppisserver.api
 
-import exception.EntityNotFoundException
 import nat20.kamppisserver.domain.InviteResponse
-import nat20.kamppisserver.domain.RoomProfileInvite
-import nat20.kamppisserver.domain.toInviteResponse
-import nat20.kamppisserver.repository.UserRepository
-import nat20.kamppisserver.repository.RoomProfileInviteRepository
-import nat20.kamppisserver.repository.RoomProfileRepository
-import nat20.kamppisserver.security.JwtUtils
 import nat20.kamppisserver.service.InviteService
-import nat20.kamppisserver.service.RoomProfileService
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/invites")
 class InviteController(
     private val inviteService: InviteService,
-    private val roomProfileService: RoomProfileService,
-    private val roomProfileInviteRepository: RoomProfileInviteRepository,
-    private val userRepository: UserRepository,
-    private val roomProfileRepository: RoomProfileRepository,
-    private val jwtUtils: JwtUtils
 ) {
 
     @PostMapping("/generate-invitetoken/{roomProfileId}")
     fun addRoomProfileInvite(@PathVariable roomProfileId: Long): ResponseEntity<InviteResponse> {
-        var invite: RoomProfileInvite? = inviteService.findInviteByRoomProfileId(roomProfileId)
-
-        // Check if invite already exists
-        if (invite != null) {
-            val inviteResponse = toInviteResponse(invite)
-            inviteResponse.message = "Invite already exists, use code ${inviteResponse.inviteToken}"
-            return ResponseEntity(inviteResponse, HttpStatus.CONFLICT)
-        }
-
-        invite = inviteService.generateAndSaveRoomInvite(roomProfileId)
-        val inviteJoinResponse = toInviteResponse(invite)
-        inviteJoinResponse.message = "Invite created successfully!"
-
-        return ResponseEntity(inviteJoinResponse, HttpStatus.OK)
+        val inviteResponsePair = inviteService.createInviteOrReturnExistingInvite(roomProfileId)
+        return ResponseEntity(inviteResponsePair.first, inviteResponsePair.second)
     }
 
     @PutMapping("/join/{inviteToken}")
     fun joinRoom(@PathVariable inviteToken: String, @RequestHeader(HttpHeaders.AUTHORIZATION) authToken: String): ResponseEntity<InviteResponse> {
-        val invite = roomProfileInviteRepository.findInviteByInviteToken(inviteToken)
-            ?: throw EntityNotFoundException("Invite with token $inviteToken not found")
-
-        val inviteResponse = toInviteResponse(invite)
-
-        // Check if invite is expired
-        if (invite.expiresAt.isBefore(LocalDateTime.now())) {
-            inviteResponse.message = "Invite code ${invite.roomProfileInviteToken} has expired"
-
-            return ResponseEntity(inviteResponse, HttpStatus.GONE)
-        }
-
-        //Get userId from JWT token
-        val token = authToken.removePrefix("Bearer ").trim()
-        val email = jwtUtils.validateTokenAndGetEmail(token)
-        val userIdAddedToRoom = userRepository.findByEmail(email!!)!!.id!!
-
-        val roomProfileId = invite.roomProfileId
-
-        // Check that user is not already added to the room
-        if (roomProfileService.findUsersRoomProfiles(roomProfileId, userIdAddedToRoom).isNullOrEmpty()) {
-            val updatedRoom = inviteService.generateRoomProfileRequest(roomProfileId, userIdAddedToRoom)
-
-            roomProfileService.update(updatedRoom, roomProfileId)
-            inviteResponse.message = "Room joined successfully"
-
-            return ResponseEntity(inviteResponse, HttpStatus.OK)
-        }
-
-        inviteResponse.message = "User already added to the room"
-
-        return ResponseEntity(inviteResponse, HttpStatus.CONFLICT)
+        val inviteResponsePair = inviteService.joinRoom(inviteToken, authToken)
+        return ResponseEntity(inviteResponsePair.first, inviteResponsePair.second)
     }
 }
--- src/main/kotlin/nat20/kamppisserver/service/InviteService.kt
+++ src/main/kotlin/nat20/kamppisserver/service/InviteService.kt
@@ -1,10 +1,15 @@
 package nat20.kamppisserver.service
 
+import exception.EntityNotFoundException
+import nat20.kamppisserver.domain.InviteResponse
+import nat20.kamppisserver.domain.toInviteResponse
 import nat20.kamppisserver.domain.RoomProfileInvite
 import nat20.kamppisserver.domain.RoomProfileRequest
 import nat20.kamppisserver.repository.RoomProfileInviteRepository
 import nat20.kamppisserver.repository.RoomProfileRepository
 import nat20.kamppisserver.repository.UserRepository
+import nat20.kamppisserver.security.JwtUtils
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import kotlin.random.Random
@@ -13,7 +18,9 @@ import kotlin.random.Random
 class InviteService(
     private val roomProfileInviteRepository: RoomProfileInviteRepository,
     private val roomProfileRepository: RoomProfileRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val roomProfileService: RoomProfileService,
+    private val jwtUtils: JwtUtils
 ) {
 
     fun findInviteByRoomProfileId(roomProfileId: Long): RoomProfileInvite? {
@@ -46,5 +53,49 @@ class InviteService(
         roomProfile.users.add(userRepository.findById(userId).get())
 
         return roomProfile.toRoomProfileRequest()
+    }
+
+    fun createInviteOrReturnExistingInvite(roomProfileId: Long): Pair<InviteResponse, HttpStatus> {
+        val existingInvite = findInviteByRoomProfileId(roomProfileId)
+
+        // Check if invite already exists
+        return if (existingInvite != null) {
+            val inviteResponse = toInviteResponse(existingInvite)
+            inviteResponse.message = "Invite already exists, use code ${inviteResponse.inviteToken}"
+            Pair(inviteResponse, HttpStatus.CONFLICT)
+        } else {
+            val newInvite = generateAndSaveRoomInvite(roomProfileId)
+            val inviteResponse = toInviteResponse(newInvite)
+            inviteResponse.message = "Invite created successfully!"
+            Pair(inviteResponse, HttpStatus.OK)
+        }
+    }
+
+    fun joinRoom(inviteToken: String, authToken: String): Pair<InviteResponse, HttpStatus> {
+        val invite = roomProfileInviteRepository.findInviteByInviteToken(inviteToken)
+            ?: throw EntityNotFoundException("Invite with token $inviteToken not found")
+
+        // Get userId from JWT token
+        val token = authToken.removePrefix("Bearer ").trim()
+        val email = jwtUtils.validateTokenAndGetEmail(token)
+        val userIdAddedToRoom = userRepository.findByEmail(email!!)!!.id!!
+        val roomProfileId = invite.roomProfileId
+
+        return if (invite.expiresAt.isBefore(LocalDateTime.now())) {
+            // Check if invite is expired
+            val inviteResponse = toInviteResponse(invite)
+            inviteResponse.message = "Invite code ${invite.roomProfileInviteToken} has expired"
+            Pair(inviteResponse, HttpStatus.GONE)
+        } else if (roomProfileService.findUsersRoomProfiles(roomProfileId, userIdAddedToRoom).isNullOrEmpty()) {
+            val updatedRoom = generateRoomProfileRequest(roomProfileId, userIdAddedToRoom)
+            roomProfileService.update(updatedRoom, roomProfileId)
+            val inviteResponse = toInviteResponse(invite)
+            inviteResponse.message = "Room joined successfully"
+            Pair(inviteResponse, HttpStatus.OK)
+        } else {
+            val inviteResponse = toInviteResponse(invite)
+            inviteResponse.message = "User already added to the room"
+            Pair(inviteResponse, HttpStatus.CONFLICT)
+        }
     }
 }
